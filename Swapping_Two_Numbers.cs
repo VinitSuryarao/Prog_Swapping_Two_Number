1a103ca75c8f561d3e4c22fa2b9ec488b1c4edb7
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:04:09 +0530

    Add files via upload

---
+++ Swapping_Two_Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Swapping Two Number
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 8;
+            int temp;
+
+            // Method 1
+            temp = a;
+            a = b;
+            b = temp;
+
+            // Method 2
+            a = a + b;
+            b = a - b;
+            b = a - b;
+
+        }
+    }
+
+
